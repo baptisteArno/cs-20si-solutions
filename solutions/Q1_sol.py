45804b00f63357edfbecc184ad4afcaeed6eb891
Baptiste Arnaud <baptiste.arnaud@parrot.com>
2017-06-15 17:00:16 +0200

    [DEV] Started Q1

--- solutions/Q1_sol.py
+++ solutions/Q1_sol.py
@@ -5,6 +5,8 @@ You should thoroughly test your code
 
 import tensorflow as tf
 
+sess = tf.InteractiveSession()
+
 ###############################################################################
 # 1a: Create two random 0-d tensors x and y of any distribution.
 # Create a TensorFlow object that returns x + y if x > y, and x - y otherwise.
@@ -14,7 +16,7 @@ import tensorflow as tf
 
 x = tf.random_uniform([])  # Empty array as shape creates a scalar.
 y = tf.random_uniform([])
-out = tf.cond(tf.less(x, y), lambda: tf.add(x, y), lambda: tf.sub(x, y))
+out = tf.cond(tf.greater(x,y), lambda: tf.add(x,y), lambda: tf.subtract(x,y))
 
 ###############################################################################
 # 1b: Create two 0-d tensors x and y randomly selected from -1 and 1.
@@ -22,7 +24,10 @@ out = tf.cond(tf.less(x, y), lambda: tf.add(x, y), lambda: tf.sub(x, y))
 # Hint: Look up tf.case().
 ###############################################################################
 
-# YOUR CODE
+x = tf.random_uniform([], -1, 1)
+y = tf.random_uniform([], -1, 1)
+
+out = tf.cond(tf.less(x,y), lambda: tf.add(x,y), lambda: tf.subtract(x,y))
 
 ###############################################################################
 # 1c: Create the tensor x of the value [[0, -2, -1], [0, 1, 2]] 
@@ -31,7 +36,9 @@ out = tf.cond(tf.less(x, y), lambda: tf.add(x, y), lambda: tf.sub(x, y))
 # Hint: Look up tf.equal().
 ###############################################################################
 
-# YOUR CODE
+x = tf.constant([[0, -2, -1], [0, 1, 2]])
+y = tf.zeros([2, 3], dtype=tf.int32)
+out = tf.equal(x,y)
 
 ###############################################################################
 # 1d: Create the tensor x of value 
@@ -46,7 +53,13 @@ out = tf.cond(tf.less(x, y), lambda: tf.add(x, y), lambda: tf.sub(x, y))
 # Hint: Use tf.gather().
 ###############################################################################
 
-# YOUR CODE
+x = tf.constant([29.05088806,  27.61298943,  31.19073486,  29.35532951,
+				 30.97266006,  26.67541885,  38.08450317,  20.74983215,
+				 34.94445419,  34.45999146,  29.06485367,  36.01657104,
+				 27.88236427,  20.56035233,  30.20379066,  29.51215172,
+				 33.71149445,  28.59134293,  36.05556488,  28.66994858])
+y = tf.where(tf.greater(x,30))
+out = tf.gather(x, y)
 
 ###############################################################################
 # 1e: Create a diagnoal 2-d tensor of size 6 x 6 with the diagonal values of 1,
@@ -54,7 +67,7 @@ out = tf.cond(tf.less(x, y), lambda: tf.add(x, y), lambda: tf.sub(x, y))
 # Hint: Use tf.range() and tf.diag().
 ###############################################################################
 
-# YOUR CODE
+x = tf.diag(tf.range(1,7))
 
 ###############################################################################
 # 1f: Create a random 2-d tensor of size 10 x 10 from any distribution.
@@ -62,7 +75,8 @@ out = tf.cond(tf.less(x, y), lambda: tf.add(x, y), lambda: tf.sub(x, y))
 # Hint: Look at tf.matrix_determinant().
 ###############################################################################
 
-# YOUR CODE
+x = tf.random_uniform([10,10])
+y = tf.matrix_determinant(x)
 
 ###############################################################################
 # 1g: Create tensor x with value [5, 2, 3, 5, 10, 6, 2, 3, 4, 2, 1, 1, 0, 9].
@@ -70,7 +84,8 @@ out = tf.cond(tf.less(x, y), lambda: tf.add(x, y), lambda: tf.sub(x, y))
 # Hint: use tf.unique(). Keep in mind that tf.unique() returns a tuple.
 ###############################################################################
 
-# YOUR CODE
+x = tf.constant([5, 2, 3, 5, 10, 6, 2, 3, 4, 2, 1, 1, 0, 9])
+y, y_ = tf.unique(x)
 
 ###############################################################################
 # 1h: Create two tensors x and y of shape 300 from any normal distribution,
@@ -82,4 +97,6 @@ out = tf.cond(tf.less(x, y), lambda: tf.add(x, y), lambda: tf.sub(x, y))
 # Hint: see the Huber loss function in the lecture slides 3.
 ###############################################################################
 
-# YOUR CODE
+x = tf.random_uniform([300])
+y = tf.random_uniform([300])
+out = tf.cond(tf.)
